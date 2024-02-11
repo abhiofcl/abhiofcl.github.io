043fcf8359c6e45fea99abb20b3ed99917413c50
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-02-11 23:41:57 +0530

    contact form submission completed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const form = document.getElementById("contactform"); // Adjust ID as needed
+// const form = document.getElementById("contactform"); // Adjust ID as needed
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); // Stops default form submission
-  alert("Message sent succesfully");
-  form.reset();
-});
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault(); // Stops default form submission
+//   alert("Message sent succesfully");
+//   form.reset();
+// });
