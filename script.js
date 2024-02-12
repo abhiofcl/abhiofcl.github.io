20ab762a1cf931e7c56d7441b0233416b6d7980d
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2024-02-12 20:42:03 +0530

    changes

--- script.js
+++ script.js
@@ -5,3 +5,21 @@
 //   alert("Message sent succesfully");
 //   form.reset();
 // });
+
+//different animations
+const leftFade = "show-from-left-blur";
+const rightFade = "show-from-right-blur";
+
+const hiddenElement = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add(leftFade);
+    } else {
+      entry.target.classList.remove(leftFade);
+    }
+  });
+});
+
+hiddenElement.forEach((el) => observer.observe(el));
